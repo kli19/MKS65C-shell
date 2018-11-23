c2da87fc2c5f9fed6807b04c2f0a7f09cb49ebcd
Karen Li <kli19@stuy.edu>
2018-11-22 22:55:57 -0500

    refactored code

--- shell.c
+++ shell.c
@@ -5,70 +5,71 @@
 # include <sys/wait.h>
 # include <fcntl.h>
 
+//parses a single command
 char ** parse_args( char * line ) {
-  //printf("Went to parse_args\n");
   char ** arr = malloc(5 * sizeof(char *));
   int i = 0;
   while(line){
     arr[i] = strsep(&line, " ");
     i++;
-    //printf("While works in parse_args\n\n");
   }
   return arr;
 }
 
+//strips string of unwanted whitespace
 char * strip_string(char * line){
   //strips whitespace in beginning of string
   while((*line) == ' '){
     line++;
-    //printf("%s\n", line); 
   }
   return line;
 }
 
+//executes commands
 void execute(){
+  //allocates space to read in commands
   char * line = malloc(5 * sizeof(char));
   scanf("%[^\n]%*c", line);
 
+  //so the code doesn't break when there is no command
   if(!line){
     return;
   }
-
+ 
   while(line){
+    //separates the line into individual commands
     char * command = strsep(&line, ";");
+
+    //strip the unwanted whitespace
     command = strip_string(command);
+
+    //read the arguments of the command into an array
     char ** args = parse_args(command);
     int f = fork();
+
+    //something went wrong with forking
     if(f==-1){
       printf("error");
     }
 
+    //execute the command in the child process
     if(f==0){
       execvp(args[0], args);
     }
+
+    //wait until the child process is done
     int status;
     wait(&status);
   }
+
 }
 
 
 int main(){
-  printf("%s\n", strip_string("      hello"));
+
   while(1){
-    int f = fork();
-    
-    if(f==-1){
-      printf("error");
-    }
-
-    if(f==0){
-      printf("Enter Command: ");
-      execute();
-      return f;
-    }
-
-    int status;
-    wait(&status);
+    printf("shell$: ");
+    execute();
   }
 
   return 0;
