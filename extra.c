7b677205d7f6304461f92809d73726b78d810ee5
alexliu4 <aliu9@stuy.edu>
2018-11-28 01:06:03 -0500

    imported a getch()

--- extra.c
+++ extra.c
@@ -49,6 +49,19 @@ void history(char * line){
   }
 }
 
+int getch(void){
+  int ch;
+  struct termios oldt;
+  struct termios newt;
+  tcgetattr(STDIN_FILENO, &oldt); /*store old settings */
+  newt = oldt; /* copy old settings to new settings */
+  newt.c_lflag &= ~(ICANON | ECHO); /* make one change to old settings in new settings */
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt); /*apply the new settings immediatly */
+  ch = getchar(); /* standard getchar call */
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt); /*reapply the old settings */
+  return ch; /*return received char */
+}
+
 void retrieve(){
   int ch1, ch2;
   printf("PRESS A KEY TO CONTINUE \n");
